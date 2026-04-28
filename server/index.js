9a1b19444447193f8af4081a1ab4b3d0a5ae10f3
IT24101694 <IT24101694@my.sliit.lk>
2026-04-28 17:13:08 +0530

    fi

--- server/index.js
+++ server/index.js
@@ -4,7 +4,9 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173'
+}));
 app.use(express.json());
 
 const booksRouter = require('./routes/books');
